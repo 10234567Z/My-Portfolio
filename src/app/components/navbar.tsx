3b79fec7bd3ab5ac7d744def3d88b7d7f54994f3
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-04-18 11:13:36 +0530

    Remove socials from navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -46,12 +46,6 @@ export default function Navbar() {
                                 >
                                     Projects
                                 </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                                >
-                                    Socials
-                                </a>
                             </div>
                         </div>
                     </div>
